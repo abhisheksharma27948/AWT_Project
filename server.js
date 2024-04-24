a9103d81f7380b2e00b85c2d809cf7d3aa0249b8
Abhishek Sharma <92991046+abhisheksharma27948@users.noreply.github.com>
2024-04-25 02:01:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ dotenv.config();
 connectDB();
 
 //esModeule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
