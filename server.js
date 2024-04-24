368f4f681d4b24713f16b305a2ef587343d3c439
Abhishek Sharma <92991046+abhisheksharma27948@users.noreply.github.com>
2024-04-25 02:04:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api/v1/product", productRoutes);
 // });
 
 app.use('*',function(req, res){
-  res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname,'./frontend/dist/index.html'));
 })
 
 //PORT
